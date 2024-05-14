f5ce1afd0ebb25dbd649c569877b1ec1e8379f61
nguyenhuuluan1702 <nguyenhuuluantvtc@gmail.com>
2024-05-14 20:30:35 +0700

    Xử lí logic cho các chức năng thêm, xóa, sửa loại sản phẩm

---
+++ Issues6_Sprint2/frmDMLoaiTraiCay.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace QuanLyBanTraiCay
+{
+    partial class frmDMLoaiTraiCay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDMLoaiTraiCay));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnTKLoaiTC = new System.Windows.Forms.Button();
+            this.txtTenLoaiTraiCay = new System.Windows.Forms.TextBox();
+            this.txtMaLoaiTraiCay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvLoaiTraiCay = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoaiTraiCay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnDong);
+            this.panel1.Controls.Add(this.btnBoQua);
+            this.panel1.Controls.Add(this.btnLuu);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnThem);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(0, 491);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1067, 63);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDong
+            // 
+            this.btnDong.BackColor = System.Drawing.Color.Transparent;
+            this.btnDong.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDong.BackgroundImage")));
+            this.btnDong.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDong.Location = new System.Drawing.Point(845, 20);
+            this.btnDong.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(100, 28);
+            this.btnDong.TabIndex = 5;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = false;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.BackColor = System.Drawing.Color.Transparent;
+            this.btnBoQua.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBoQua.BackgroundImage")));
+            this.btnBoQua.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBoQua.Location = new System.Drawing.Point(699, 20);
+            this.btnBoQua.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(100, 28);
+            this.btnBoQua.TabIndex = 4;
+            this.btnBoQua.Text = "Bỏ Qua";
+            this.btnBoQua.UseVisualStyleBackColor = false;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackColor = System.Drawing.Color.Transparent;
+            this.btnLuu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLuu.BackgroundImage")));
+            this.btnLuu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLuu.Location = new System.Drawing.Point(552, 20);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(100, 28);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.Color.Transparent;
+            this.btnSua.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSua.BackgroundImage")));
+            this.btnSua.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSua.Location = new System.Drawing.Point(405, 20);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(100, 28);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.Color.Transparent;
+            this.btnXoa.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnXoa.BackgroundImage")));
+            this.btnXoa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnXoa.Location = new System.Drawing.Point(259, 20);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(100, 28);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.Transparent;
+            this.btnThem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnThem.BackgroundImage")));
+            this.btnThem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnThem.Location = new System.Drawing.Point(112, 20);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(100, 28);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.btnTKLoaiTC);
+            this.panel2.Controls.Add(this.txtTenLoaiTraiCay);
+            this.panel2.Controls.Add(this.txtMaLoaiTraiCay);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1067, 176);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnTKLoaiTC
+            // 
+            this.btnTKLoaiTC.BackColor = System.Drawing.Color.Transparent;
+            this.btnTKLoaiTC.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTKLoaiTC.BackgroundImage")));
+            this.btnTKLoaiTC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTKLoaiTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTKLoaiTC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTKLoaiTC.Location = new System.Drawing.Point(452, 119);
+            this.btnTKLoaiTC.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTKLoaiTC.Name = "btnTKLoaiTC";
+            this.btnTKLoaiTC.Size = new System.Drawing.Size(145, 36);
+            this.btnTKLoaiTC.TabIndex = 5;
+            this.btnTKLoaiTC.Text = "Thống kê";
+            this.btnTKLoaiTC.UseVisualStyleBackColor = false;
+            this.btnTKLoaiTC.Click += new System.EventHandler(this.btnTKLoaiTC_Click);
+            // 
+            // txtTenLoaiTraiCay
+            // 
+            this.txtTenLoaiTraiCay.Location = new System.Drawing.Point(259, 130);
+            this.txtTenLoaiTraiCay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTenLoaiTraiCay.Name = "txtTenLoaiTraiCay";
+            this.txtTenLoaiTraiCay.Size = new System.Drawing.Size(132, 22);
+            this.txtTenLoaiTraiCay.TabIndex = 4;
+            // 
+            // txtMaLoaiTraiCay
+            // 
+            this.txtMaLoaiTraiCay.Location = new System.Drawing.Point(259, 78);
+            this.txtMaLoaiTraiCay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMaLoaiTraiCay.Name = "txtMaLoaiTraiCay";
+            this.txtMaLoaiTraiCay.Size = new System.Drawing.Size(132, 22);
+            this.txtMaLoaiTraiCay.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(79, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên Loại Trái Cây";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(79, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã Loại Trái Cây";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(425, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(357, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh Mục Loại Trái Cây";
+            // 
+            // dgvLoaiTraiCay
+            // 
+            this.dgvLoaiTraiCay.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvLoaiTraiCay.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvLoaiTraiCay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLoaiTraiCay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLoaiTraiCay.Location = new System.Drawing.Point(0, 176);
+            this.dgvLoaiTraiCay.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvLoaiTraiCay.Name = "dgvLoaiTraiCay";
+            this.dgvLoaiTraiCay.RowHeadersWidth = 51;
+            this.dgvLoaiTraiCay.Size = new System.Drawing.Size(1067, 315);
+            this.dgvLoaiTraiCay.TabIndex = 2;
+            this.dgvLoaiTraiCay.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLoaiTraiCay_CellClick);
+            this.dgvLoaiTraiCay.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLoaiTraiCay_CellContentClick);
+            // 
+            // frmDMLoaiTraiCay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dgvLoaiTraiCay);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmDMLoaiTraiCay";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Danh mục loại trái cây";
+            this.Load += new System.EventHandler(this.frmDMLoaiTraiCay_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoaiTraiCay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Button btnBoQua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvLoaiTraiCay;
+        private System.Windows.Forms.TextBox txtTenLoaiTraiCay;
+        private System.Windows.Forms.TextBox txtMaLoaiTraiCay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTKLoaiTC;
+    }
+}
---
+++ Issues6_Sprint2/frmDMLoaiTraiCay.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //Sử dụng thư viện để làm việc SQL server
+using QuanLyBanTraiCay.Class; //Sử dụng class Functions.cs
+
+namespace QuanLyBanTraiCay
+{
+    public partial class frmDMLoaiTraiCay : Form
+    {
+        DataTable tblLTC; //Chứa dữ liệu bảng Loại Trái Cây
+        public frmDMLoaiTraiCay()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDMLoaiTraiCay_Load(object sender, EventArgs e)
+        {
+            txtMaLoaiTraiCay.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = false;
+            LoadDataGridView(); //Hiển thị bảng tblChatLieu
+            dgvLoaiTraiCay.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void LoadDataGridView()
+        {
+            string sql;
+            sql = "SELECT* FROM tblLoaiTraiCay";
+            tblLTC = Class.Functions.GetDataToTable(sql); //Đọc dữ liệu từ bảng
+            dgvLoaiTraiCay.DataSource = tblLTC; //Nguồn dữ liệu            
+            dgvLoaiTraiCay.Columns[0].HeaderText = "Mã Loại Trái Cây";
+            dgvLoaiTraiCay.Columns[1].HeaderText = "Tên Loại Trái Cây";
+            dgvLoaiTraiCay.Columns[0].Width = 100;
+            dgvLoaiTraiCay.Columns[1].Width = 300;
+            dgvLoaiTraiCay.AllowUserToAddRows = false; //Không cho người dùng thêm dữ liệu trực tiếp
+            dgvLoaiTraiCay.EditMode = DataGridViewEditMode.EditProgrammatically; //Không cho sửa dữ liệu trực tiếp
+        }
+
+        private void dgvLoaiTraiCay_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+            if (btnThem.Enabled == false)
+            {
+                MessageBox.Show("Đang ở chế độ thêm mới!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaLoaiTraiCay.Focus();
+                return;
+            }
+            if (tblLTC.Rows.Count == 0) //Nếu không có dữ liệu
+            {
+                MessageBox.Show("Không có dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            txtMaLoaiTraiCay.Text = dgvLoaiTraiCay.CurrentRow.Cells["MaLoaiTraiCay"].Value.ToString();
+            txtTenLoaiTraiCay.Text = dgvLoaiTraiCay.CurrentRow.Cells["TenLoaiTraiCay"].Value.ToString();
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnBoQua.Enabled = true;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnBoQua.Enabled = true;
+            btnLuu.Enabled = true;
+            btnThem.Enabled = false;
+            ResetValue(); //Xoá trắng các textbox
+            txtMaLoaiTraiCay.Enabled = true; //cho phép nhập mới
+            txtMaLoaiTraiCay.Focus();
+        }
+
+        private void ResetValue()
+        {
+            txtMaLoaiTraiCay.Text = "";
+            txtMaLoaiTraiCay.Text = "";
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql; //Lưu lệnh sql
+            if (txtMaLoaiTraiCay.Text.Trim().Length == 0) //Nếu chưa nhập mã chất liệu
+            {
+                MessageBox.Show("Bạn phải nhập mã loại trái cây", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaLoaiTraiCay.Focus();
+                return;
+            }
+            if (txtTenLoaiTraiCay.Text.Trim().Length == 0) //Nếu chưa nhập tên chất liệu
+            {
+                MessageBox.Show("Bạn phải nhập tên loại trái cây", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenLoaiTraiCay.Focus();
+                return;
+            }
+            sql = "Select MaLoaiTraiCay From tblLoaiTraiCay where MaLoaiTraiCay=N'" + txtMaLoaiTraiCay.Text.Trim() + "'";
+            if (Class.Functions.CheckKey(sql))
+            {
+                MessageBox.Show("Mã loại trái cây này đã có, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaLoaiTraiCay.Focus();
+                return;
+            }
+
+            sql = "INSERT INTO tblLoaiTraiCay VALUES(N'" +
+                txtMaLoaiTraiCay.Text + "',N'" + txtTenLoaiTraiCay.Text + "')";
+            Class.Functions.RunSQL(sql); //Thực hiện câu lệnh sql
+            LoadDataGridView(); //Nạp lại DataGridView
+            ResetValue();
+            btnXoa.Enabled = true;
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnBoQua.Enabled = false;
+            btnLuu.Enabled = false;
+            txtMaLoaiTraiCay.Enabled = false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string sql; //Lưu câu lệnh sql
+            if (tblLTC.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaLoaiTraiCay.Text == "") //nếu chưa chọn bản ghi nào
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtTenLoaiTraiCay.Text.Trim().Length == 0) //nếu chưa nhập tên chất liệu
+            {
+                MessageBox.Show("Bạn chưa nhập tên loại trái cây", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            sql = "UPDATE tblLoaiTraiCay SET TenLoaiTraiCay=N'" +
+                txtTenLoaiTraiCay.Text.ToString() +
+                "' WHERE MaLoaiTraiCay=N'" + txtMaLoaiTraiCay.Text + "'";
+            Class.Functions.RunSQL(sql);
+            LoadDataGridView();
+            ResetValue();
+
+            btnBoQua.Enabled = false;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblLTC.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaLoaiTraiCay.Text == "") //nếu chưa chọn bản ghi nào
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Bạn có muốn xoá không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                sql = "DELETE tblLoaiTraiCay WHERE MaLoaiTraiCay=N'" + txtMaLoaiTraiCay.Text + "'";
+                Class.Functions.RunSQL(sql);
+                LoadDataGridView();
+                ResetValue();
+            }
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            ResetValue();
+            btnBoQua.Enabled = false;
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            txtMaLoaiTraiCay.Enabled = false;
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgvLoaiTraiCay_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (btnThem.Enabled == false)
+            {
+                MessageBox.Show("Đang ở chế độ thêm mới!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaLoaiTraiCay.Focus();
+                return;
+            }
+            if (tblLTC.Rows.Count == 0) //Nếu không có dữ liệu
+            {
+                MessageBox.Show("Không có dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            txtMaLoaiTraiCay.Text = dgvLoaiTraiCay.CurrentRow.Cells["MaLoaiTraiCay"].Value.ToString();
+            txtTenLoaiTraiCay.Text = dgvLoaiTraiCay.CurrentRow.Cells["TenLoaiTraiCay"].Value.ToString();
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnBoQua.Enabled = true;
+        }
+        private int DemSoHangLoaiTraiCay()
+        {
+            int soHang = 0;
+            // Chuỗi kết nối đến cơ sở dữ liệu
+            string connectionString = "Data Source=DESKTOP-1R2Q7BD;Initial Catalog=Quanlybantraicay3;Integrated Security=True";
+
+            // Tạo đối tượng SqlConnection
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string sql = "SELECT COUNT(*) FROM tblLoaiTraiCay";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    soHang = (int)command.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in DemSoHangLoaiTraiCay: " + ex.Message);
+                // Xử lý lỗi khi thực hiện truy vấn
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return soHang;
+        }
+        private void btnTKLoaiTC_Click(object sender, EventArgs e)
+        {
+            int soHang = DemSoHangLoaiTraiCay();
+            MessageBox.Show("Số loại trái cây là: " + soHang, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+    }
